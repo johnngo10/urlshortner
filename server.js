b3316f91f41e7392d5fe7e46ee25b25e5a0ae30d
xuanngo10 <xuan.ngo10@gmail.com>
2020-11-30 10:33:14 -0800

    connected mongodb, mongoose & created schema

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose');
+const shortUrl = require('./models/shorturl');
 
-// Basic Configuration
-const port = process.env.PORT || 3000;
+// Setup mongodb connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => console.log('connected to db'))
+  .catch((err) => console.log(err));
 
 app.use(cors());
 
@@ -18,6 +22,20 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.use(express.urlencoded({extended: false}))
+
+app.post('/api/shorturl/new', function(req, res) {
+  const fullUrl = req.body.url;
+  console.log(`Requested URL: ${fullUrl}`);
+
+  const record = new shortUrl({
+    original_url: fullUrl
+  })
+})
+
+// Basic Configuration
+const port = process.env.PORT || 3000;
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
